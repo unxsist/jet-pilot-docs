c972a775f9c6e520e03e12656a63b068839fee65
Jeroen Nijhuis <jeroen.nijhuis@justeattakeaway.com>
2024-04-22 08:18:19 +0200

    ci: enable debug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  debug: true,
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxtjs/color-mode",
